5bf5969c7316f1802bcb77c7ec66de8d199f62d9
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-05-05 19:07:18 -0700

    Updates

---
+++ precache-manifest.97c788a99f3183e96778b8f8d4b70ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641e369a8e61b795f01d2ed9a4cba487",
+    "url": "/yacht/index.html"
+  },
+  {
+    "revision": "0d2ce28abd9347d0e8bc",
+    "url": "/yacht/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "023d1f95ffb85795e089",
+    "url": "/yacht/static/js/2.5503395e.chunk.js"
+  },
+  {
+    "revision": "819d680c1f74790e84a4b7dc37ae019d",
+    "url": "/yacht/static/js/2.5503395e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2ce28abd9347d0e8bc",
+    "url": "/yacht/static/js/main.ab177d79.chunk.js"
+  },
+  {
+    "revision": "a09e2521142dfd90d2dc",
+    "url": "/yacht/static/js/runtime-main.40742a09.js"
+  }
+]);
